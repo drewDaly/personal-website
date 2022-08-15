e447e2637fbfb495e1e005b34e3080a21a7d73e2
Drew Daly <ad120136.drew@gmail.com>
2022-08-15 17:29:29 -0500

    terminal started with no commands

--- src/components/Command.js
+++ src/components/Command.js
@@ -3,25 +3,34 @@ import "./Command.scss"
 const Command = () => {
     var prevCommands = [];
     var curr;
-
     const handleKeyPress = useCallback((event) => {
-        if (event.keyCode === 13 && event.target.value !== "") {
+      if (event.keyCode === 13) event.preventDefault();
+      if (event.keyCode === 13 && event.target.value !== "") {
+          event.preventDefault();
           prevCommands.push(event.target.value);
-          // handle command
-          // add line
-          curr = prevCommands.length;
+          event.target.disabled = true;
+          var elem = document.getElementsByClassName('command')[prevCommands.length - 1];
+          var clone = elem.cloneNode(true);
+          clone.className = "command";
+          clone.firstChild.innerHTML = "visitor@drew.com:~$&nbsp;";
+          clone.firstChild.style = 'p';
+          clone.firstChild.style.color = "#F1D6AB";
           event.target.value = "";
+          event.target.disabled = false;
+          elem.before(clone);
+          // handle command
+          curr = prevCommands.length;
         }
         if (event.keyCode === 38 && curr !== 0) {
-          --curr;
+          curr -= 1;
           event.target.value = prevCommands[curr];
         }
         if (event.keyCode === 40 && curr !== prevCommands.length) {
-          ++curr;
+          curr += 1;
           if (prevCommands[curr] === undefined) event.target.value = "";
           else event.target.value = prevCommands[curr];
         }
-    })
+    },[])
     useEffect(() => {
       window.addEventListener('keydown', handleKeyPress);
       return () => {
@@ -30,8 +39,11 @@ const Command = () => {
     }, [handleKeyPress])
     
     return (
-    <div className="command">
-        <textarea onKeyDown={handleKeyPress} type='text' id='commandline'></textarea>
+    <div className="commmand-wrap" id="command-wrap">
+    <span className="command" id="command">
+        <div className='user'><p>visitor@drew.com:~$&nbsp;</p></div>
+        <textarea onKeyDown={handleKeyPress} type='text' id='commandline' spellCheck='false'></textarea>
+    </span>
     </div>
   )
 }
