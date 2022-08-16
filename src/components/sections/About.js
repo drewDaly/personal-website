919f54c48cdc04d91ef2ac31584fe0d9d298cffe
Drew Daly <ad120136.drew@gmail.com>
2022-08-15 19:51:15 -0500

    did some stuff to about section

--- src/components/sections/About.js
+++ src/components/sections/About.js
@@ -1,6 +1,23 @@
-import React from 'react'
+import React, {useEffect, useRef} from 'react'
+import profile from "./../images/20210117_132027.jpg";
+import profile2 from "./../images/20210117_131631.jpg"
 import "./About.scss"
 function About() {
+  const imgRef = useRef();
+  const listenHover = (event) => {
+    imgRef.current.src = profile2;
+  }
+  const listenLeave = (event) => {
+    imgRef.current.src = profile;
+  }
+  useEffect(() => {
+    window.addEventListener('mouseenter', listenHover);
+  
+    return () => {
+      window.removeEventListener('mouseenter', listenHover);
+    }
+  }, [])
+  
   return (
     <div className="about-container">
         <h2 className="about-title">
@@ -10,6 +27,9 @@ function About() {
         <p className="about-para">
             I do stuff and like things
         </p>
+        <div className="aboutImg-wrap">
+          <img ref={imgRef} onMouseEnter={listenHover} onMouseLeave={listenLeave} src={profile} alt="profile" style={{width: "100%"}} id="profile"/>
+        </div>
         </div>
     </div>
   )
